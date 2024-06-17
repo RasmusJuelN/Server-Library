772c358a46d010e7b25133b553f73d8688831922
RasmusJuelN <rasmusn686@hotmail.com>
2024-06-17 11:04:25 +0200

    register page layout and routing

--- ServerLibraryAngular/src/app/Components/Accounts/register/register.component.ts
+++ ServerLibraryAngular/src/app/Components/Accounts/register/register.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthService } from '../../Services/auth.service';
-
+import { AuthService } from '../../../Services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ ReactiveFormsModule, RouterModule],
+  imports: [ ReactiveFormsModule, RouterModule, MatButtonModule, MatRippleModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
@@ -19,9 +20,12 @@ export class RegisterComponent {
     ) { }
 
     protected registerForm = new FormGroup({
-      password: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required])
+      firstName: new FormControl ('', [Validators.required]),
+      lastName: new FormControl ('', [Validators.required]),
+      email: new FormControl ('', [Validators.required]),
+      password: new FormControl ('', [Validators.required])
     })
+
 
     onRegister(){
       if(this.registerForm.valid){
--- ServerLibraryAngular/src/app/Components/navbar/navbar.component.ts
+++ ServerLibraryAngular/src/app/Components/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, RouterModule, MatButtonModule, MatRippleModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
